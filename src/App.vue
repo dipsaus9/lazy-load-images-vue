38ac108cf4bd5dfbd27183da89aa49354091e7e2
Dennis <dennis.spierenburg@hotmail.com>
2020-10-21 10:39:08 +0200

    Example of lazy load directive

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+	<div>
+		<img v-lazy alt="Vue logo" data-src="./logo.png" />
+		<img v-lazy alt="Vue logo" data-src="./logo2.png" />
+		<img v-lazy alt="Vue logo" data-src="./logo3.png" />
+		<img v-lazy alt="Vue logo" data-src="./logo4.png" />
+		<img v-lazy alt="Vue logo" data-src="./logo5.png" />
+		<img v-lazy alt="Vue logo" data-src="./logo6.png" />
+		<img v-lazy alt="Vue logo" data-src="./logo7.png" />
+	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld
-  }
+	name: "App"
 });
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+div {
+	display: flex;
+	flex-direction: column;
+}
+img {
+	padding: 50vh 0;
+	width: 20%;
+	margin: 0 auto;
+	height: auto;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,24 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const LazyLoadDirective = {
+	mounted(el: HTMLImageElement) {
+		const imageObserver = new IntersectionObserver(entries => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					const lazyImage = entry.target as HTMLElement;
+					if (lazyImage.dataset.src) {
+						el.src = lazyImage.dataset.src;
+						imageObserver.unobserve(el);
+					}
+				}
+			});
+		});
+		imageObserver.observe(el);
+	}
+};
+
+const app = createApp(App);
+
+app.directive("lazy", LazyLoadDirective);
+app.mount("#app");
